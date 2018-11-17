cf67afb54899c1ec3570d785147b144a27615997
Juniver Sinambela <juniversinambela@gmail.com>
2018-11-17 20:39:12 +0700

    Updates

---
+++ precache-manifest.981d92513c978905e3da8f1920f6d647.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/juniversinambela/notify-gojek/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ffd81ea31ca531070034",
+    "url": "/juniversinambela/notify-gojek/static/js/runtime~main.ffd81ea3.js"
+  },
+  {
+    "revision": "3d956d9f66f6c29cfb94",
+    "url": "/juniversinambela/notify-gojek/static/js/main.3d956d9f.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/juniversinambela/notify-gojek/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "3d956d9f66f6c29cfb94",
+    "url": "/juniversinambela/notify-gojek/static/css/main.1a819f0d.chunk.css"
+  },
+  {
+    "revision": "95d2b03aafaec35bbe401809c0fd926e",
+    "url": "/juniversinambela/notify-gojek/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/juniversinambela/notify-gojek/precache-manifest.981d92513c978905e3da8f1920f6d647.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/juniversinambela/notify-gojek/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
